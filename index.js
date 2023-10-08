45674452514965c3349a36e2810b5f158c2c355c
Manishcodelive <manishmaurya13678@gmail.com>
2023-10-08 10:45:59 +0530

    first commi

---
+++ index.js
@@ -0,0 +1,12 @@
+ function calc(value){
+document.getElementById("result").value += value
+}
+function calculate() {
+let p = document.getElementById("result").value
+let q = eval(p);
+  document.getElementById("result").value = q;
+  } 
+function clearScreen() {
+document.getElementById("result").value = "";
+}
+  
